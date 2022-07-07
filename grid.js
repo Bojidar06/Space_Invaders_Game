7681146affaa58ddbee40fb2e2a1d110bfd1d725
Bozhidar Sotirov <bojkoburgas@abv.bg>
2022-07-07 03:56:54 +0300

    Add files via upload

---
+++ grid.js
@@ -0,0 +1,33 @@
+import Invador from './invador.js'
+import Player from './player.js'
+export default class Grid{
+    constructor(){
+        this.position = {
+            x: 0, 
+            y: 0
+        }
+
+        this.velocity = {
+            x: 5,
+            y: 0
+        }
+
+        this.invadors = []
+
+        this.rows = Math.floor(Math.random() * 4 + 2)
+        this.columns = Math.floor(Math.random() * 9 + 5)
+        this.check = false
+        this.width = this.columns * 25
+
+        for(let x = 0; x < this.columns; ++x){
+            for(let y = 0; y < this.rows; ++y)
+                this.invadors.push(new Invador({
+                    position: {
+                        x: x * 33,
+                        y: y * 33
+                    }
+                }))
+            }
+        }
+}
+
